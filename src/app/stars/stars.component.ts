0fb0a6cfdec7c14a274b62404cc93bc8e7a7d48c
qingfengwuyou <h3658686@126.com>
2017-07-02 23:24:23 +0800

    no message

---
+++ src/app/stars/stars.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-stars',
+  templateUrl: './stars.component.html',
+  styleUrls: ['./stars.component.css']
+})
+export class StarsComponent implements OnInit {
+
+  @Input()
+  rating: number;
+
+  // 三星 [true,true,true,false,false]
+  stars: boolean[] = [];
+
+
+  constructor() { }
+
+  ngOnInit() {
+    for (let i = 1; i <= 5; i++) {
+      this.stars.push(this.rating >= i);
+    }
+  }
+
+}
