e00af7abbe62b666407438541433b869f8c624a0
qingfengwuyou <h3658686@126.com>
2017-07-14 22:38:52 +0800

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import {Routes, RouterModule} from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -8,6 +9,26 @@ import { CarouselComponent } from './carousel/carousel.component';
 import { ProductComponent } from './product/product.component';
 import { FooterComponent } from './footer/footer.component';
 import { StarsComponent } from './stars/stars.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HomeComponent } from './home/home.component';
+
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'product/:title',
+    component: ProductDetailComponent
+  }
+];
+
 
 @NgModule({
   declarations: [
@@ -17,10 +38,13 @@ import { StarsComponent } from './stars/stars.component';
     CarouselComponent,
     ProductComponent,
     FooterComponent,
-    StarsComponent
+    StarsComponent,
+    ProductDetailComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/product-detail/product-detail.component.ts
@@ -0,0 +1,20 @@
+import {ActivatedRoute} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+
+  productTitle: string;
+
+  constructor(private routeInfo: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.productTitle = this.routeInfo.snapshot
+      .params.title;
+  }
+
+}
